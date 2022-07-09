2a1e42a477942fd3a2032addc7c86291c506eaf9
sc18am <sc18am@leeds.ac.uk>
2022-07-09 16:21:04 +0300

    Adds report to Firestore Database - All fields including current user logged on UID and email

--- PotholeManager/ViewControllers/HomeViewController.swift
+++ PotholeManager/ViewControllers/HomeViewController.swift
@@ -19,18 +19,46 @@ class HomeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     
     @IBOutlet weak var label: UILabel!
     
+    
+    @IBOutlet weak var streetTextField: UITextField!
+    
+    
+    @IBOutlet weak var cityTextField: UITextField!
+    
+    
+    @IBOutlet weak var residentialDistrictTextField: UITextField!
+    
+    
+    @IBOutlet weak var postcodeTextField: UITextField!
+    
+    
+    @IBOutlet weak var widthTextField: UITextField!
+    
+    
+    @IBOutlet weak var depthTextField: UITextField!
+    
+    
+    @IBOutlet weak var enterDetailsTextField: UITextField!
+    
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
     private let storage = Storage.storage().reference()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //getUserDetails()
+        errorLabel.alpha = 0
+        getUserDetails()
         
     }
     
     
     
-  /**  func getUserDetails() {
+    func getUserDetails() -> (uid: String, email: String?) {
         
         // Get the details of the current user logged on.
         let user = Auth.auth().currentUser
@@ -39,9 +67,13 @@ class HomeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
             let uid = user.uid
             let email = user.email
             
+            return (uid, email)
+            
         }
         
-    } **/
+        return ("","")
+        
+    }
     
   
     @IBAction func uploadPhotoTapped(_ sender: Any) {
@@ -54,7 +86,24 @@ class HomeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         
     }
     
+    @IBAction func submitReportButtonTapped(_ sender: Any) {
+        
+        addReportToDatabase(globalPath: globalPath)
+        
+    }
+    
+    
     var globalPath = ""
+    
+    
+    // Error message to be displayed whenever there is an error.
+    func showError(_ message:String){
+        
+        errorLabel.text = message
+        errorLabel.alpha = 1
+        
+    }
+    
     
     // Called when user finishes picking a photo.
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
@@ -124,12 +173,71 @@ class HomeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         
     }
 
+    
+    
+    
+    // Check fields and validate that data is correct. If correct then return nil else return error message.
+    func validateFields() -> String? {
+        
+        // Check that necessary fields are filled in.
+        if streetTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            cityTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            residentialDistrictTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            postcodeTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            
+            return "Please fill in all required fields."
+            
+        }
+        
+        return nil
+    }
+    
+    
     func addReportToDatabase(globalPath: String) {
         
-        // Save a reference to the file in Firestore DB
-        let db = Firestore.firestore()
-        db.collection("images").document().setData(["url":self.globalPath])
         
+        // Checking the fields are correct.
+        let error = validateFields()
+        
+        
+        // If there is an error show the error message.
+        if error != nil {
+            
+            errorLabel.text = error!
+            errorLabel.alpha = 1
+            
+        }
+        else {
+            
+            // Store the report in the database. Get all the data.
+            let street = streetTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let city = cityTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let residentialDistrict = residentialDistrictTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let postcode = postcodeTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let width = widthTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let depth = depthTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let enterDetails = enterDetailsTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let uid = getUserDetails().uid
+            let email = getUserDetails().email!
+           
+            
+            // Save a reference to the file in Firestore DB
+            let db = Firestore.firestore()
+            //db.collection("reports").document().setData(["url":self.globalPath])
+            
+            db.collection("reports").addDocument(data: ["url": globalPath, "street": street, "city": city, "residentialdistrict": residentialDistrict, "postcode": postcode, "width": width, "depth": depth, "details": enterDetails, "uid": uid, "email": email]) { (error) in
+
+                if error != nil {
+                    
+                    //Show error message.
+                    self.showError("Error uploading the report to the database.")
+                }
+            }
+            
+            print("Succesful")
+            
+        }
     }
+    
     
 }
